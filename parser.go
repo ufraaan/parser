c6f1795bb0715eebd221719778f0e6b9a2b663cb
ufraaan <ufraansiddiqui30@gmail.com>
2026-05-15 16:06:56 +0530

    extract title, meta description, and links

--- parser.go
+++ parser.go
@@ -2,25 +2,46 @@ package parser
 
 import (
 	"net/http"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
-func Parse(url string) (string, error) {
+type Page struct {
+	Title       string
+	Description string
+	Links       []string
+}
+
+func Parse(url string) (Page, error) {
 	res, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return Page{}, err
 	}
 
 	defer res.Body.Close()
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		return "", err
+		return Page{}, err
+	}
+	page := Page{}
+
+	// get the title
+	page.Title = doc.Find("title").Text()
+
+	// get the meta description content
+	desc, exists := doc.Find("meta[name=description]").Attr("content")
+	if exists {
+		page.Description = desc
 	}
 
-	title := doc.Find("title").Text()
+	// get urls
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		href, exists := s.Attr("href")
+		if exists {
+			page.Links = append(page.Links, href)
+		}
+	})
 
-	return title, nil
+	return page, nil
 }
-
-
