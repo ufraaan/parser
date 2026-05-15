f9908f43c73603454af6f832a91f13a7442dfd1a
ufraaan <ufraansiddiqui30@gmail.com>
2026-05-15 21:27:21 +0530

    accept url arg and print full page details

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ufraaan/parser"
 )
 
 func main() {
-	text := "Hello, world!! Go_lang is-fast. 123_test, okay???"
-	arr, err := parser.Tokenise(text) 
-	if err != nil {
-		fmt.Print("error: ", err)
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: parser <url>")
+		os.Exit(1)
 	}
-	fmt.Println(arr)
+
+	url := os.Args[1]
+	page, body, err := parser.Parse(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("title:", page.Title)
+	fmt.Println("description:", page.Description)
+	fmt.Println("links:")
+	for _, l := range page.Links {
+		fmt.Println("  ", l)
+	}
+
+	tokens, _ := parser.Tokenise(body)
+	fmt.Println("tokens:", tokens)
 }
 
 
